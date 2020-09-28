4c01d44259e57d9a612cbdaf7caf192c728cb60a
Cory <coryrosser@gmail.com>
2020-09-28 14:21:12 -0400

    selector and styling covered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //selector can be done in multiple ways
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    p {
+      color: dodgerblue;
+    }  
+  `]
 })
 export class AppComponent {
   name = 'CR';
