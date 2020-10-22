4a9f05bc12dc9a18ea95194deb2b41040642f77a
Cory <coryrosser@gmail.com>
2020-10-21 23:17:04 -0400

    wed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -35,3 +35,77 @@ export class ServersComponent implements OnInit {
     this.serverName.length > 1 ? false : true
   }
 }
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // template: '<app-server></app-server><app-server></app-server>'
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  // allowNewServer = false;
+  serverCreationStatus = 'No server was created.'
+  serverName = '';
+  serverCreated = false;
+  servers= ['Test Server', 'Test 2 Server']
+
+  constructor() { 
+    // setTimeout(()=>{
+    //   this.allowNewServer = true
+    // }, 2000)
+  }
+
+  ngOnInit(): void {
+  }
+  onUpdateServerName(e) {
+    console.log(e.target.value)
+    this.serverName = (<HTMLInputElement>e.target).value
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created. ' + 'Server name is: ' + this.serverName
+    this.servers.push(this.serverName)
+    this.serverName = ''
+    this.serverCreated = true
+  }
+  allowNewServer() {
+    this.serverName.length > 1 ? false : true
+  }
+}
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // template: '<app-server></app-server><app-server></app-server>'
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  // allowNewServer = false;
+  serverCreationStatus = 'No server was created.'
+  serverName = '';
+  serverCreated = false;
+  servers= ['Test Server', 'Test 2 Server']
+
+  constructor() { 
+    // setTimeout(()=>{
+    //   this.allowNewServer = true
+    // }, 2000)
+  }
+
+  ngOnInit(): void {
+  }
+  onUpdateServerName(e) {
+    console.log(e.target.value)
+    this.serverName = (<HTMLInputElement>e.target).value
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created. ' + 'Server name is: ' + this.serverName
+    this.servers.push(this.serverName)
+    this.serverName = ''
+    this.serverCreated = true
+  }
+  allowNewServer() {
+    this.serverName.length > 1 ? false : true
+  }
+}
